990b84f040f145c75c8883bcdf34911437be349c
e759ksh <e759ksh@naver.com>
2022-12-06 19:30:41 +0900

    comment 삭제 기능 추가

--- src/api/comment.js
+++ src/api/comment.js
@@ -9,7 +9,7 @@ CommentRouter.post("/", authenticateUser, async (req, res) => {
     const doc = await Comment.create({
       ...req.body,
       createdBy: req.user._id,
-      nickname: req.user.nickname
+      nickname: req.user.nickname,
     });
     res.status(201).json({ data: doc });
   } catch (error) {
@@ -36,6 +36,26 @@ CommentRouter.get("/:id", async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(400).json({ message: "sth wrong", error });
+  }
+});
+
+CommentRouter.delete("/:id", authenticateUser, async (req, res) => {
+  try {
+    res.header("Access-Control-Allow-Origin", "*");
+    const removed = await Comment.findOneAndRemove({
+      _id: req.params.id,
+      createdBy: req.user._id,
+    })
+      .lean()
+      .exec();
+    if (!removed) {
+      return res.status(400).json({ message: "cannot remove the data" });
+    }
+
+    return res.status(200).json({ ...removed });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "sth wrong", error });
   }
 });
 
